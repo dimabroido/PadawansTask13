e8d649bce464321abfcec2eee8d00a0024cff108
dima <dima.broido@gmail.com>
2019-02-16 13:31:23 +0300

    task13 publicTestsDone
    task13 publicTestsDone

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Employee
+{
+    private string Surname;
+    private int Age;
+
+    public Employee()
+    {
+        Surname = string.Empty;
+        Age = 0;
+    }
+
+    public Employee(string s, int a)
+    {
+        Surname = s;
+        Age = a;
+    }
+
+    public Employee(string s)
+    {
+        Surname = s;
+    }
+
+    private string GetAge()
+    {
+        return "Age: {Employee.age}";
+    }
+
+
+    public void ChangeEmployeeSurname()
+    {
+        Employee NewSurname = new Employee();
+
+        
+    }
+
+
+
+    public string GetEmployeeInfo()
+    {
+        return "Surname: {Employee.surname}, Age: {Employee.age}";
+    }
+}
